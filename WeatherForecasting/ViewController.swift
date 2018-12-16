02e0e8020317815f2aa368cefce3011dedcb52fe
José Victor <josevictorpatos@gmail.com>
2018-12-15 22:15:44 -0200

    Initial Commit

---
+++ WeatherForecasting/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeatherForecasting
+//
+//  Created by José Victor Pereira Costa on 15/12/18.
+//  Copyright © 2018 José Victor Pereira Costa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
